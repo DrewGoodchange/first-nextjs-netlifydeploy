06280e5da53990076c99958e4c7e83d840352069
DrewGoodchange <drew@goodchange.app>
2023-11-22 17:30:11 -0600

    readded parameters

---
+++ netlify/functions/roundup.js
@@ -0,0 +1,10 @@
+exports.handler = async (event, context) => {
+    try {
+        
+        const response = await fetch('')
+    } catch (error) {
+        console.log(error);
+        return
+    }
+
+}
