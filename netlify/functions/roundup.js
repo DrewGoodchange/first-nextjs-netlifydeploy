e09815bad66b03f667a7dcbc6e39ee0d857e3c1e
DrewGoodchange <drew@goodchange.app>
2023-11-22 18:50:30 -0600

    created roundup function

--- netlify/functions/roundup.js
+++ netlify/functions/roundup.js
@@ -1,10 +1,36 @@
-exports.handler = async (event, context) => {
+exports.handler = async (req, res) => {
     try {
+
+        // Parse the incoming data from the API payload
+        const data = JSON.parse(req.body);
+        console.log('This is data: ', data);
+
+        /* Start of roundup function */
+
+        // Assign the incoming transactions array to a variable, don't forget to take away
+        const transArray = [397, 123, 265, 54, 431, 178, 309, 92, 487, 76];
         
-        const response = await fetch('')
+        // map over transactions, roundup each value
+        const roundupArray = transArray.map(num => Math.ceil(num / 100) * 100);
+
+        // Find the difference between the sum of roundupArray and transactions
+        const transSum = transArray.reduce((acc, curr) => acc + curr);
+        const roundupSum = roundupArray.reduce((acc, curr) => acc + curr);
+        const differenceOfSums = roundupSum - transSum;
+
+        console.log('transArray = ', transArray);
+        console.log('roundupArray = ', roundupArray);
+        console.log('differenceOfSums = ', differenceOfSums);
+        
+        // return an obj with transactions
+        return {
+            transHistory: transArray,
+            roundup: roundupArray,
+            sum: differenceOfSums
+        };
+
     } catch (error) {
         console.log(error);
         return
-    }
-
-}
+    };
+};
